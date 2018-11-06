ea11bca36584ad5c81e05a31691e4cb211131c18
Stefan Nilsson <stefan.ef@gmail.com>
2018-11-06 21:16:49 +0100

    First testcafe tests

--- e2e-playground/testcafe/tests/svt.spec.ts
+++ e2e-playground/testcafe/tests/svt.spec.ts
@@ -5,11 +5,6 @@ const svt = new SVTPage();
 fixture `Testa SVT`
     .page `https://www.svt.se`;
 
-    test('Huvudrubriken säger att Sverige vunnit VM-guld', async t => {
-        await t
-            .expect(svt.huvudRubrik.innerText).eql('Sverige vinner VM-guld!');
-    });
-
     test('Går att navigera till Lokala nyheter för Stockholm', async t => {
         await t
             .click(svt.menyLokalt)
@@ -21,7 +16,13 @@ fixture `Testa SVT`
         await t
             .click(svt.menyAllaProgram)
             .typeText(svt.allaProgramSök, 'Antikmagasinet')
+            .pressKey('enter')
             .expect(svt.allaProgramFörstaResultatet.innerText).eql('Antikmagasinet')
             .click(svt.allaProgramFörstaResultatet)
             .expect(svt.programTitel.innerText).eql('Antikmagasinet');
+    });
+
+    test('Huvudrubriken på förstasidan säger att Sverige vunnit VM-guld', async t => {
+        await t
+            .expect(svt.huvudRubrik.innerText).eql('Sverige vinner VM-guld!');
     });
